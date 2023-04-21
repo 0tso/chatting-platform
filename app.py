fc1c58548512befd90f69429ff5f4b2f61411254
Otso <24369914+0tso@users.noreply.github.com>
2023-04-21 21:25:12 +0300

    Default socketio pinging options added

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-socketio = SocketIO(app, ping_interval=int(getenv("SOCKETIO_PING_INTERVAL")), ping_timeout=int(getenv("SOCKETIO_TIMEOUT")))
+socketio = SocketIO(app,
+                    ping_interval=int(getenv("SOCKETIO_PING_INTERVAL",  default=5)),
+                    ping_timeout=int(getenv("SOCKETIO_TIMEOUT",         default=5)))
 
 import routes
 import sockets
